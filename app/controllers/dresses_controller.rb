6399b49e8ed7a803f58a33e53347aa04ec89ca58
Vladislav Radonjic <vanjar@gmail.com>
2019-02-10 19:24:34 +0000

    Dodao stranice za svaku haljinu u bazi, i link sa dress indexa. Dodao navigaciju na vencanice.

--- app/controllers/dresses_controller.rb
+++ app/controllers/dresses_controller.rb
@@ -3,6 +3,10 @@ class DressesController < ApplicationController
 		@dresses = Dress.all
 	end
 
+	def show
+		@dress = Dress.find(params[:id])
+	end
+
 	def new
 		@dress = Dress.new
 	end
@@ -14,12 +18,28 @@ class DressesController < ApplicationController
 			if @dress.save
 				format.html {redirect_to new_dress_path, notice: 'Nova haljina je snimljena'}
 			else
-				format.html {render :new}
+				format.html { render :new }
 			end
+		end
+	end
+
+	def edit
+		@dress = Dress.find(params[:id])
+	end
+
+	def update
+		respond_to do |format|
+			if @dress.update(dress_params)
+				format.html { redirect_to dresses_path, notice: 'Izmene su sacuvane.'}
+			else
+				format.html { render :edit }
+			end
+		end
 	end
 
 	private
 		def dress_params
 			params.require(:dress).permit(:name, :short_desc, :long_desc, :kwrds, :main_img, :main_img)
 		end
+
 end
